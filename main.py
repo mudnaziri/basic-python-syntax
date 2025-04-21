fde633a9ba73f470bc66fa757b716c78a6cd5611
Muhammad Muddasir <nazirimuddasir@gmail.com>
2025-04-21 15:58:34 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+#from operations.addition import addition
+#print(addition(88,99,55))
+
+#from operations.division import division
+#print(division(3,5,6))
+
+#from operations.multiplication import multiplication
+#print(multiplication(5,5,5))
+
+#from operations.subtraction import subtraction
+#print(subtraction(44,55,64))
+
+#from operations.dices_game import dice_roll
+#print(dice_roll(2))
+
+#from file import square 
+#print(square(9))
+
+
+#from file import lbs 
+#print(lbs(156))  
+
+#from operations.meter_centimeter import meter
+#print(meter(8))
+
