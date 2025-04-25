f4e59eca9dde3a50b0efa72374732c4d8ad32539
Muhammad Muddasir <nazirimuddasir@gmail.com>
2025-04-25 15:10:04 +0500

    Add files via upload

---
+++ program.py
@@ -0,0 +1,15 @@
+d = input("distance in KM: ")
+t = input("time in hour: ")                   # v = d / t
+velocity = int(d) / int(t) 
+print(velocity)
+
+i = input("current in ampire: ")
+r = input("risistance in ohm: ")                # v = i * r
+potential_difference =int(i) * int(r)
+print(potential_difference)
+
+#file = open("mud.txt", "r")
+#content = file.read()
+#print(content)
+#file.close()
+
